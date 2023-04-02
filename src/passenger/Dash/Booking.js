a900509966c6a65443940947359c09c8aa053e4e
thushari <s17376@sci.pdn.ac.lk>
2023-04-02 21:57:32 +0530

    searched vehicle UI

--- src/passenger/Dash/Booking.js
+++ src/passenger/Dash/Booking.js
@@ -4,11 +4,11 @@ import { Col, Row, Card, Container, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Header from "../../Components/Header";
 import axiosInstance from "../../axios/axios-instance";
+// import VehicleCard from "./VehicleCard";
 // import Map from "../../components/Map";
 export default function Booking() {
-  const vehicle = ["None..", "Car", "Van", "Bus", "Cab"];
+  const vehicle = ["CAR", "VAN", "BUS"];
   const towns = [
-    "None..",
     "Colombo",
     "Kurunegala",
     "Kandy",
@@ -71,126 +71,158 @@ export default function Booking() {
 
   return (
     <div>
-      <Header />
-      <Container className="ms-0 mt-5">
-        <Row>
-          <Col style={{ marginLeft: "-12px", display: "contents" }}>
-            <PassengerDashSideBar />
-          </Col>
-          <Col>
-            <Card className="mt-5 ms-5 border-dark border-opacity-50">
-              <Card.Header
-                as="h5"
-                className="text-center text-white-50 fw-bold"
-                style={{ backgroundColor: "darkslateblue" }}
-              >
-                Search Vehicles
-              </Card.Header>
-              <Card.Body
-                className="ms-3 me-3 mb-3"
-                style={{ background: "#bfb8de", color: "darkslateblue" }}
-              >
-                <Form className="container">
-                  <Row>
-                    <Col>
-                      <Form.Group controlId="reservationForm.vehicleType">
-                        <Form.Label className="mb-2 mx-1 mt-5">
-                          Vehicle Type
-                        </Form.Label>
-                        <Form.Select
-                          className="mb-4"
-                          onChange={(event) => setType(event.target.value)}
-                        >
-                          {SelectVehicle}
-                        </Form.Select>
-                      </Form.Group>
-                    </Col>
-                    <Col>
-                      <Form.Group controlId="reservationForm.noOfPassengers">
-                        <Form.Label className="mb-2 mx-1 mt-5">
-                          No. Of Passengers
-                        </Form.Label>
-                        <Form.Control
-                          onChange={(event) =>
-                            setPassangers(event.target.value)
-                          }
-                          type="number"
-                          className="mb-4"
-                        />
-                      </Form.Group>
-                    </Col>
-                  </Row>
-
-                  <Row>
-                    <Col>
-                      <Form.Group controlId="reservationForm.date">
-                        <Form.Label className="mb-2 mx-1">Date</Form.Label>
-                        <Form.Control
-                          onChange={(event) => setDate(event.target.value)}
-                          type="date"
-                          className="mb-4"
-                        />
-                      </Form.Group>
-                    </Col>
-                    <Col>
-                      <Form.Group controlId="reservationForm.noOfDays">
-                        <Form.Label className="mb-2 mx-1">
-                          For how many days?
-                        </Form.Label>
-                        <Form.Control
-                          type="number"
-                          placeholder="Maximum is 4"
-                          onChange={(event) => setDates(event.target.value)}
-                          className="mb-4"
-                        />
-                      </Form.Group>
-                    </Col>
-                  </Row>
-
-                  <Row>
-                    {/* <Col>
-                      <Form.Group controlId="reservationForm.time">
-                        <Form.Label className="mb-2 mx-1">Time</Form.Label>
-                        <Form.Control type="time" className="" />
-                      </Form.Group>
-                    </Col> */}
-                    <Col>
-                      <Form.Group
-                        controlId="reservationForm.nearestTowns"
-                        className="mb-xl-5"
+      <div className="me-5 mt-5 row">
+        <Col style={{ marginLeft: "-12px", display: "contents" }}>
+          <PassengerDashSideBar />
+        </Col>
+        <Col className="col-6">
+          <Card className="mt-5 ms-5 border-opacity-50 ">
+            <Card.Header
+              as="h5"
+              className="text-center text-white-50 fw-bold"
+              style={{ backgroundColor: "darkslateblue" }}
+            >
+              Search Vehicles
+            </Card.Header>
+            <Card.Body
+              className="ms-3 me-3 mb-3"
+              style={{ background: "#bfb8de", color: "darkslateblue" }}
+            >
+              <Form className="container">
+                <Row>
+                  <Col>
+                    <Form.Group controlId="reservationForm.vehicleType">
+                      <Form.Label className="mb-2 mx-1 mt-5">
+                        Vehicle Type
+                      </Form.Label>
+                      <Form.Select
+                        className="mb-4"
+                        onChange={(event) => setType(event.target.value)}
                       >
-                        <Form.Label className="mb-2 mx-1">
-                          Nearest Towns
-                        </Form.Label>
-                        <Form.Select
-                          onChange={(event) => setTown(event.target.value)}
-                          className=""
-                        >
-                          {selectNearestTowns}
-                        </Form.Select>
-                      </Form.Group>
-                    </Col>
-                  </Row>
-                  {/* <Row>
+                        {SelectVehicle}
+                      </Form.Select>
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group controlId="reservationForm.noOfPassengers">
+                      <Form.Label className="mb-2 mx-1 mt-5">
+                        No. Of Passengers
+                      </Form.Label>
+                      <Form.Control
+                        onChange={(event) => setPassangers(event.target.value)}
+                        type="number"
+                        className="mb-4"
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col>
+                    <Form.Group controlId="reservationForm.date">
+                      <Form.Label className="mb-2 mx-1">Date</Form.Label>
+                      <Form.Control
+                        onChange={(event) => setDate(event.target.value)}
+                        type="date"
+                        className="mb-4"
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group controlId="reservationForm.noOfDays">
+                      <Form.Label className="mb-2 mx-1">
+                        For how many days?
+                      </Form.Label>
+                      <Form.Control
+                        type="number"
+                        placeholder="Maximum is 4"
+                        onChange={(event) => setDates(event.target.value)}
+                        className="mb-4"
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col>
+                    <Form.Group
+                      controlId="reservationForm.nearestTowns"
+                      className="mb-xl-5"
+                    >
+                      <Form.Label className="mb-2 mx-1">
+                        Nearest Towns
+                      </Form.Label>
+                      <Form.Select
+                        onChange={(event) => setTown(event.target.value)}
+                        className=""
+                      >
+                        {selectNearestTowns}
+                      </Form.Select>
+                    </Form.Group>
+                  </Col>
+                </Row>
+                {/* <Row>
                     <Map/>
                   </Row> */}
 
-                  <Link className="mx-auto mb-4">
-                    <Button
-                      type="button"
-                      className="text-center text-white-50"
-                      onClick={submit}
-                      style={{ backgroundColor: "darkslateblue" }}
-                    >
-                      Search Vehicle
-                    </Button>
-                  </Link>
-                </Form>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+                <Link className="mx-auto mb-4">
+                  <Button
+                    type="button"
+                    className="text-center text-black"
+                    onClick={submit}
+                    // style={{ backgroundColor: "darkslateblue" }}
+                    variant="outline-secondary"
+                  >
+                    Search Vehicle
+                  </Button>
+                </Link>
+              </Form>
+            </Card.Body>
+          </Card>
+
+          <div className="d-flex ms-3">
+            <div className="mt-5 mb-5">
+              <Card style={{ width: "18rem" }} className="shadow ms-2 me-5">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title className="text-center">Available</Card.Title>
+                  <Card.Text className="sm font fst-italic">
+                    Before Your Booking Please Contact Driver And Make Comfermation
+                  </Card.Text>
+                  <ul>Vehicle Type</ul>
+                  <ul>Vehicle Number</ul>
+                  <ul>Driver Name</ul>
+                  <ul>Telephone Number</ul>
+                  <ul>Email</ul>
+                  <Button variant="outline-secondary" className="w-50 ms-5">
+                    Book
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="mt-5 mb-5">
+              <Card style={{ width: "18rem" }} className="shadow">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title className="text-center">Available</Card.Title>
+                  <Card.Text className="sm font fst-italic">
+                  Before Your Booking Please Contact Driver And Make Comfermation
+                  </Card.Text>
+                  <ul>Vehicle Type</ul>
+                  <ul>Vehicle Number</ul>
+                  <ul>Driver Name</ul>
+                  <ul>Telephone Number</ul>
+                  <ul>Email</ul>
+                  <Button variant="outline-secondary" className="w-50 ms-5">
+                    Book
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+        </Col>
+      </div>
     </div>
   );
 }
--- src/passenger/Dash/PassengerDashSideBar.js
+++ src/passenger/Dash/PassengerDashSideBar.js
@@ -2,30 +2,32 @@ import React from "react";
 import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export default function PassengerDashSideBar() {
-  return (
-    <div className="">
-      <div style={{ height: "25px", backgroundColor:" #bfb8de"}}></div>
-      <div
-        className=" high pe-5"
-        style={{ minHeight: "530px", maxWidth:"275px" , backgroundColor:" #bfb8de"}}
-      >
-        <Nav className="flex-column p-5">
-          <Nav className="pb-3 fw-bold gx-xl-4">
-            <Link to={"/Passenger/Profile"} style={{color:"darkblue"}}>Profile</Link>
-          </Nav>
-          <Nav className="pb-3 fw-bold gx-xl-4">
-            <Link to={"/Booking"} style={{color:"darkblue"}}>Booking</Link>
-          </Nav>
-          <Nav className="pb-3 fw-bold gx-xl-4">
-            <Link to={"/Passenger/Hist"} style={{color:"darkblue"}}>User Histry</Link>
-          </Nav>
-          {/* <Nav className="pb-3 fw-bold gx-xl-4">
-            <Link to={"/Reservation/Map"} style={{color:"darkblue"}}>Map</Link>
-          </Nav> */}
-        </Nav>
-      </div>
-    </div>
-  );
+export default function DriveDashSideBar() {
+    return (
+        <div>
+            <div
+                className="mt-3"
+                style={{ minHeight: "100%" ,zIndex:'1', maxWidth: "275px", backgroundColor: " #bfb8de" }}
+            >
+                <Nav className="flex-column p-5">
+                    <Nav className="pb-3 fw-bold gx-xl-4">
+                        <Link to={"/Passenger/Profile"} style={{ color: "darkblue" }}>Profile</Link>
+                    </Nav>
 
+                    <Nav className="pb-3 fw-bold gx-xl-4">
+                        <Link to={"/Booking"} style={{ color: "darkblue" }}>Booking</Link>
+                    </Nav>
+
+                    <Nav className="pb-3 fw-bold gx-xl-4">
+                        <Link to={"/Passenger/Hist"} style={{ color: "darkblue" }}>Histry</Link>
+                    </Nav>
+
+                    {/* <Nav className="pb-3 fw-bold gx-xl-4">
+                        <Link to={"/Reservation/Map"} style={{ color: "darkblue" }}>Map</Link>
+                    </Nav> */}
+
+                </Nav>
+            </div>
+        </div>
+    );
 }
